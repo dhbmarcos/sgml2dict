166699e37d04af63898d787ea434dc04ead573e4
D. H. B. Marcos <dhbmarcos@gmail.com>
2023-07-16 03:23:31 -0300

    Release.

--- setup.py
+++ setup.py
@@ -3,16 +3,16 @@ from distutils.core import setup
 setup(
   name             = "sgml2dict",
   packages         = ["sgml2dict"],
-  version          = "0.1.1",
+  version          = "0.0.4",
   license          = "MIT",
   description      = "Convert SGML text to dictionary.",
   author           = "D. H. B. Marcos",
   author_email     = "dhbmarcos@gmail.com",
   url              = "https://gitlab.com/dhbmarcos/sgml2dict",
-  download_url     = "https://gitlab.com/dhbmarcos/sgml2dict/-/archive/0.0.3/sgml2dict-0.0.3.tar.gz",
+  download_url     = "https://gitlab.com/dhbmarcos/sgml2dict/-/archive/0.0.4/sgml2dict-0.0.4.tar.gz",
   keywords         = ["SGML", "DICT", "XML", "HTML"],
   install_requires = [],
-  classifiers=[
+  classifiers      = [
     "Development Status :: 3 - Alpha",
     "Intended Audience :: Developers",
     "Topic :: Software Development :: Build Tools",
